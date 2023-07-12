cfeead8a373841287e87a9093fc795e45bde3e4f
Like-A-Hoss <hapleswithdice@gmail.com>
2023-07-11 22:20:57 -0500

    Fire away

--- frontend/scripts/getEvents.js
+++ frontend/scripts/getEvents.js
@@ -11,25 +11,42 @@ async function getEventsList() {
     return [];
   }
 }
-
+//Creates list of events for inserting into website.  Will figure out how to make this a list.
 function renderEvents(events) {
   const eventList = document.getElementById('event_list');
   eventList.innerHTML = '';
 
   events.forEach((event) => {
     const li = document.createElement('li');
-    li.textContent = event.name;
 
+    // Create a link with the event name as the display text
+    const link = document.createElement('a');
+    link.textContent = event.name;
+
+    // Set the link's href to the URL of the page that displays the event by ID
+    link.href = `/events/${event.id}`;
+
+    // Append the link to the list item
+    li.appendChild(link);
+
+    // Create a sublist for the location
     const location = document.createElement('ul');
     const locationItem = document.createElement('li');
     locationItem.textContent = event.location;
     location.appendChild(locationItem);
 
+    // Append the sublist to the list item
     li.appendChild(location);
+
+    // Append the list item to the event list
     eventList.appendChild(li);
-  })
-  return eventListS;
+  });
+
+  return eventList;
 }
+
+
+//init
 const init = async () => {
     const events = await getEventsList();
     renderEvents(events);
