3d0a8024edd9bc21d23caf1c4f2027cf4e3242ae
Like-A-Hoss <hapleswithdice@gmail.com>
2023-07-14 13:58:28 -0500

    Server.test

---
+++ tests/server.test.js
@@ -0,0 +1,24 @@
+const request = require("supertest");
+const { app } = require("../server");
+
+describe("Server", () => {
+  it("should return 'Logged out' when not authenticated", async () => {
+    // Make a request to the server
+    const response = await request(app).get("/");
+
+    // Assert the response
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Logged out");
+  });
+});
+
+test("Testing Login", async () => {
+    // Some Convince this thing I have logged in?
+    //
+   // Make a request to the server
+  const response = await request(app).get("/");
+
+  // Assert the response
+  expect(response.status).toBe(200);
+  expect(response.text).toBe("Logged out");
+});
