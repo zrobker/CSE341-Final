b7be93e0779813ff15130e74d13c23e9ecd3846c
Like-A-Hoss <hapleswithdice@gmail.com>
2023-07-14 15:21:58 -0500

    Spelling error corrected.

--- tests/users.controller.test.js
+++ tests/users.controller.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const { app } = require("./server");
+const { app } = require("../server");
 const UserModel = require("../models/User").UserModel;
 const {
   getAll,
